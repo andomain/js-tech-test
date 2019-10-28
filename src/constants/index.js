e3b62233ab2cb5c3f25bdc810ef3845f31551c56
Sam Anderson <sam@andomain.co.uk>
2019-10-28 22:39:26 +0000

    Debug Websocket connection

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const SOCKET_URL = 'ws://localhost:8889';
+
+export const TYPE = {
+    SUBSCRIBE: 'subscribe',
+    GET_LIVE_EVENTS: 'getLiveEvents',
+};
--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import { SOCKET_URL, TYPE } from './constants';
+
+// Debug WS connection
+const socket = new WebSocket(SOCKET_URL);
+
+socket.onopen = () => {
+    socket.addEventListener("message", m => console.log(JSON.parse(m.data)));
+    socket.send(JSON.stringify({ type: TYPE.GET_LIVE_EVENTS, primaryMarkets: false }));
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
